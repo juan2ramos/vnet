ea76a1a1aef2a5811034c00223e9aca129d12418
DiegoMalagon <diego@altactic.com>
2013-10-31 12:23:06 -0500

    Formulario html para envio de mensajes con multiselect

--- src/AT/vocationetBundle/Controller/MensajesController.php
+++ src/AT/vocationetBundle/Controller/MensajesController.php
@@ -19,7 +19,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class MensajesController extends Controller
 {
     /**
-     * Index de la aplicacion
+     * Accion para el modulo de mensajes
      * 
      * @Route("/", name="mensajes")
      * @Template("vocationetBundle:Mensajes:index.html.twig")
@@ -31,8 +31,53 @@ class MensajesController extends Controller
         if(!$security->authentication()){ return $this->redirect($this->generateUrl('login'));} 
 //        if(!$security->authorization($this->getRequest()->get('_route'))){ throw $this->createNotFoundException($this->get('translator')->trans("Acceso denegado"));}
         
+        $form = $this->createMensajeForm();
         
-        return array();
+        
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+    
+    /**
+     * Accion para enviar mensajes
+     * 
+     * @Route("/enviar_mensajes", name="enviar_mensajes")
+     * @Method({"POST"})
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return Response
+     */
+    public function enviarMensajeAction(Request $request)
+    {
+        
+        return new Response();
+    }
+    
+    /**
+     * Funcion para crear formulario de mensaje
+     * 
+     * @return Object formulario
+     */
+    private function createMensajeForm()
+    {
+        $mensajes = $this->get('mensajes');
+        $security = $this->get('security');
+        
+        $usuarioId = $security->getSessionValue('id');
+        $usuarios = $mensajes->getToList($usuarioId);        
+        
+        $dataForm = array(
+            'to' => null, 
+            'subject' => null,
+            'message' => null,
+        );
+        $form = $this->createFormBuilder($dataForm)
+           ->add('to', 'choice', array('required' => true, 'choices' => $usuarios, 'expanded' => false, 'multiple' => true))
+           ->add('subject', 'text', array('required' => true))
+           ->add('message', 'textarea', array('required' => true))
+           ->getForm();
+        
+        return $form;
     }
 }
 ?>
--- src/AT/vocationetBundle/Services/MensajesService.php
+++ src/AT/vocationetBundle/Services/MensajesService.php
@@ -6,7 +6,7 @@ namespace AT\vocationetBundle\Services;
  * 
  * @author Diego Malagón <diego@altactic.com>
  */
-class SecurityService
+class MensajesService
 {
     var $doctrine;
     var $session;
@@ -18,5 +18,42 @@ class SecurityService
         $this->session = $session;
         $this->mail = $mail;
     }
+    
+    /**
+     * Funcion que obtiene la lista de usuarios para enviar un mensaje
+     * 
+     * Obtiene los usuarios a los cuales el usuario logueado puede enviarle mensajes
+     * 
+     * @param integer $usuarioId id de usuario logueado
+     * @return array arreglo de usuarios
+     */
+    public function getToList($usuarioId)
+    {
+        $toList = false;
+        $em = $this->doctrine->getManager();
+        $dql = "SELECT
+                    u.id, 
+                    u.usuarioNombre,
+                    u.usuarioApellido
+                FROM 
+                    vocationetBundle:Usuarios u
+                WHERE 
+                    u.usuarioEstado = 1
+                    AND u.id != :usuarioId";
+        $query = $em->createQuery($dql);
+        $query->setParameter('usuarioId', $usuarioId);
+        $result = $query->getResult();
+        
+        if(count($result)>0)
+        {
+            foreach($result as $r)
+            {
+                $toList[$r['id']] = $r['usuarioNombre'].' '.$r['usuarioApellido'];
+            }
+        }
+        
+        return $toList;
+    }
+    
 }
 ?>
