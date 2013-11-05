914fdf64b864f1a351d16c0e49d242e57e2afaa0
CamiloQuijano <camilo@altactic.com>
2013-11-05 10:59:34 -0500

    Comentarios y respuesta de comentarios foros

--- src/AT/vocationetBundle/Controller/ForosController.php
+++ src/AT/vocationetBundle/Controller/ForosController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace AT\vocationetBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,7 +23,11 @@ class ForosController extends Controller
      * @version 1
      * @Route("c_{id}/t_{temaId}/f_{foroId}", name="foros_temas", defaults={"temaId" = 0, "foroId" = 0})
      * @Template("vocationetBundle:Foros:index.html.twig")
+     * @Template("vocationetBundle:Foros:showForo.html.twig")
      * @Method("GET")
+     * @param Int $id Id de la carrera
+     * @param Int $temaId Id del tema
+     * @param Int $foroId Id del foro
      * @return Render
      */
     public function indexAction($id, $temaId, $foroId)
@@ -38,20 +41,14 @@ class ForosController extends Controller
         $temas = $this->getTemasCountForos($id);
         $PosActual = Array('carreraId' => $id, 'temaId' => $temaId);
 
-        
         if ($foroId)
         {
 			// If hay foro seleccionado redireccionara el show del foro
-
 			$foro = $em->getRepository('vocationetBundle:Foros')->findOneById($foroId);
 			$comentarios = $this->getComentarios($foroId);
-			//$security->debug($comentarios);
-			
-
 			return $this->render('vocationetBundle:Foros:showForo.html.twig', array(
 				'carreras' => $carreras, 'temas' => $temas,	'actual' => $PosActual, 'foro' => $foro, 'comentarios' => $comentarios));
 		}
-
 
 		else
 		{
@@ -60,9 +57,6 @@ class ForosController extends Controller
 			return $this->render('vocationetBundle:Foros:index.html.twig', array(
 					'carreras' => $carreras, 'temas' => $temas,	'actual' => $PosActual,	'foros' => $foros ));
 		}
-		
-        //$carreras =$this->getCarrerasCountTemas();
-
         
         return array(
 			'carreras' => $carreras,
@@ -71,24 +65,20 @@ class ForosController extends Controller
 			'foros' => $foros);
     }
 
-
-
-
-
-/**
+	/**
      * Crear un comentario
      * 
      * Funcion que permite crear un comentario desde una peticion AJAX
-     * envia $request type, p->comentario de publicacion, d->comentario de archivo, rc->respuesta comentario
-     * envia $request id, variable dependiente a el tipo p->id de publicacion, d->id del archivo, rc->id de comentario
-     * envia $request content, variable con contenido del comentario
+     * envia $request tipo, 1=>respuesta a un comentario, 0=>Comentario principal o normal
+     * envia $request idforo, Id del forp
+     * envia $request comentario, variable con contenido del comentario
+     * envia $request idcomrespuesta, Id del comentario a responder, si es comentario principal viene con 0
      * 
-     * @author Diego Malagon
      * @author Camilo Quijano
-     * @version 2
+     * @version 1
      * @Method("POST")
-     * @Route("/comentar", name="crear_comentario")
-     * @param Request $request Request enviado desde ajax, con el id, tipo, y contenido del comentario
+     * @Route("comentar", name="crear_comentario")
+     * @param Request $request Request enviado desde ajax, con el id, tipo, id de comentario respuesta y contenido del comentario
      * @return object Vista renderizada con el comentario creado
      */
     public function comentarAction(Request $request)
@@ -97,44 +87,36 @@ class ForosController extends Controller
 		$usuarioId = $security->getSessionValue('id');
 		$usuarioImagen = $security->getSessionValue('usuarioImagen');
 		$usuarioName = $security->getSessionValue('usuarioNombre')." ".$security->getSessionValue('usuarioApellido');
-		//$ok = 0;
-		//echo 'oktest';
-		//return $ok;
 
 		$id = $request->get('idforo');
         $comentarioTexto = $request->get('comentario');
-        $tipo = $request->get('tipo'); // O-> Comentario, 1->Respuesta Comentario
+        $tipo = $request->get('tipo');
+        $comentarioRespuestaId = $request->get('idcomrespuesta');
+        
         $fechaCreacion = new \DateTime();
 
         $em = $this->getDoctrine()->getManager();
         $usuario = $em->getRepository('vocationetBundle:Usuarios')->findOneById($usuarioId);
         $foro = $em->getRepository('vocationetBundle:Foros')->findOneById($id);
 
-        //if ($tipo == 0) {
-			$newComentario = new Comentarios();
-			$newComentario->setTexto($comentarioTexto);
-			$newComentario->setCreated($fechaCreacion);
-			$newComentario->setModified($fechaCreacion);
-			$newComentario->setEstado('');
-			$newComentario->setUsuario($usuario);
-			$newComentario->setForo($foro);
-			$em->persist($newComentario);
-			$em->flush();
-			
-			$comentarioId = $newComentario->getId();
-			//Notificaciones
-		//}
+        if ($comentarioRespuestaId != 0) {
+			$comentarioRespuesta = $em->getRepository('vocationetBundle:Comentarios')->findOneById($comentarioRespuestaId);
+		} else {
+			$comentarioRespuesta = null;
+		}
 
+		$newComentario = new Comentarios();
+		$newComentario->setTexto($comentarioTexto);
+		$newComentario->setCreated($fechaCreacion);
+		$newComentario->setModified($fechaCreacion);
+		$newComentario->setEstado('');
+		$newComentario->setUsuario($usuario);
+		$newComentario->setForo($foro);
+		$newComentario->setComentario($comentarioRespuesta);
+		$em->persist($newComentario);
+		$em->flush();
 		
-
-		/*$javascript = "<script>$('.commentresponse').on('click', function(e){
-				comentId = $(this).data('comentid');
-				$('#commentresponseid'+comentId).css('display','inline');
-			});</script>";
-	*/
-	$javascript='';
-		
-
+		$comentarioId = $newComentario->getId();
         
 		$renderComentario =  $this->renderView('vocationetBundle:Foros:comentarioShow.html.twig', array(
             'usuarioId' => $usuarioId,
@@ -147,104 +129,40 @@ class ForosController extends Controller
             'foroId'=> $id,
         ));
 
-        $renderRespuesta =  $this->renderView('vocationetBundle:Foros:comentarioNew.html.twig', array(
-			'respuesta'=> 1,
-			'foroId'=> $id,
-			'comentarioId' => $comentarioId,
-			'usuarioId' => $usuarioId,
-			'imagen' => $usuarioImagen
-		));
+        if ($comentarioRespuesta) {
+			$return = $renderComentario;
+		}
+		else {
+			$renderRespuesta =  $this->renderView('vocationetBundle:Foros:comentarioNew.html.twig', array(
+				'respuesta'=> 1,
+				'foroId'=> $id,
+				'comentarioId' => $comentarioId,
+				'usuarioId' => $usuarioId,
+				'imagen' => $usuarioImagen
+			));
 
-        $return = $renderComentario."<div id='divnew1".$id.$comentarioId."'></div><div STYLE='display:none;' id='commentresponseid".$comentarioId."' >
-						".$renderRespuesta."
-					</div>".$javascript;
-		//$return = $renderComentario;
-		//return $return;
+			$return = $renderComentario."
+				<div id='divnew1".$id.$comentarioId."'></div><div STYLE='display:none;' id='commentresponseid".$comentarioId."' >
+					".$renderRespuesta."
+				</div>";
+		}
 		return new Response($return);
-		
-		//return 10;
-		/**  $security = $this->get('security');
-        if(!$security->autentication()){ return $this->redirect($this->generateUrl('cuenta_registro'));}
-        if(!$security->autorization($this->getRequest()->get('_route'))){ throw $this->createNotFoundException($this->get('translator')->trans("Acceso denegado"));}
-        $this->getRequest()->setLocale($this->get('config')->getConfigValue('USU_idioma'));
-        
-        if($request->getMethod() == 'POST')
-        {
-            $usuarioId = $security->getSessionValue('cta_usuario_id');
-            $usuarioNombre = $security->getSessionValue('cta_usuario_nombre');
-            $usuarioApellido = $security->getSessionValue('cta_usuario_apellido');
-            $usuarioThbPerfil = $security->getSessionValue('cta_usuario_thb_perfil');
-
-            $id = $request->get('id');
-            $comentarioTexto = $request->get('content');
-            $tipo = $request->get('type');
-
-            $em = $this->getDoctrine()->getManager();
-            $usuario = $em->getRepository('CuentaBundle:CtaUsuarios')->findOneById($usuarioId);
-            
-            $newComentario = new IntComentarios();
-            $newComentario->setComentarioTexto($comentarioTexto);
-            $newComentario->setComentarioFecha(new \DateTime());
-            $newComentario->setUsuario($usuario);
-            
-            if($tipo == "p"){
-                $publicacion = $em->getRepository('CuentaBundle:IntPublicaciones')->findOneById($id);
-                $newComentario->setPublicacion($publicacion);
-            }
-            else if($tipo == "d"){
-                $archivo = $em->getRepository('CuentaBundle:DocArchivos')->findOneById($id);
-                $newComentario->setArchivo($archivo);
-            }
-            else if ($tipo == "rc"){
-                $comentario = $em->getRepository('CuentaBundle:IntComentarios')->findOneById($id);
-                $newComentario->setComentario($comentario);
-                if ($comentario){
-                    $pub = $comentario->getPublicacion();
-                    if ($pub){
-                        $newComentario->setPublicacion($pub);
-                    }
-                    $arc = $comentario->getArchivo();
-                    if ($arc){
-                        $newComentario->setArchivo($arc);
-                    }
-                }
-            }
-            
-            $em->persist($newComentario);
-            $em->flush();
-
-            $this->get('puntos')->actualizarpuntos('PTOS_crear_comentario', $security);
-            
-            if($tipo == "p"){
-                $this->get('publicaciones')->notificarMencionesComentarioPublicacion($publicacion, $newComentario, $usuarioNombre.' '.$usuarioApellido, $usuarioId);
-            }
-            else if($tipo == "d"){
-                $this->get('publicaciones')->notificarMencionesComentarioArchivo($archivo, $newComentario, $usuarioNombre.' '.$usuarioApellido, $usuarioId);
-            }
-            else if($tipo == "rc"){
-                if ($pub){  $this->get('publicaciones')->notificarMencionesComentarioPublicacion($pub, $newComentario, $usuarioNombre.' '.$usuarioApellido, $usuarioId); }
-                if ($arc){  $this->get('publicaciones')->notificarMencionesComentarioArchivo($arc, $newComentario, $usuarioNombre.' '.$usuarioApellido, $usuarioId); }
-            }
-        }
-        
-        return $this->render('CuentaBundle:IntComentarios:show_ajax.html.twig', array(
-            'usuarioId' => $usuarioId,
-            'usuarioNombre' => $usuarioNombre,
-            'usuarioApellido' => $usuarioApellido,
-            'usuarioThbPerfil' => $usuarioThbPerfil,
-            'comentario' => $newComentario,
-            'tipo' => $tipo,
-            'id' => $id,
-        ));
-        * */
     }
     
-
+	/**
+	 * Comentario del foro que ingresa por parametro
+	 *
+	 * Esta funcion retorna los comentarios del foro que se ingresa por parametro organizados en dos niveles
+	 * Comentarios principal, y dentro de este, comentarios respuesta (cr)
+	 *
+	 * @author Camilo Quijano
+     * @version 1
+	 * @param Int $foroId Id del foro
+	 * @return Array Arreblo bidimensional
+	 */
     function getComentarios($foroId)
     {
 		$em = $this->getDoctrine()->getManager();
-		//SELECT * FROM comentarios WHERE foro_id = 1;
-		//$comentarios = $em->getRepository('vocationetBundle:Comentarios')->findBy(Array('foro' =>$foroId));
 		$dql = "SELECT c.id, c.texto, c.created, u.usuarioNombre, u.usuarioApellido, u.usuarioImagen, u.id AS usuarioId,
 					cr.id AS commentRespuesta
 				FROM vocationetBundle:Comentarios c
@@ -263,65 +181,14 @@ class ForosController extends Controller
 			$comentRespo = $c['commentRespuesta'];
 			if ($comentRespo)
 			{
-				//$comentRespoId = $comentRespo->getId();
 				$Objcomentarios[$comentRespo]['cr'][$comentId] = $c;
-				
-			//if (!in_Array($c, $auxKeys)) {
-				
 			}
 			else
 			{
 				$Objcomentarios[$comentId] = $c;
 			}
-			
-                /*$key = $c['id'];
-                if (!in_Array($c['comentarioRespuesta'], $auxKeys)){
-                    $comentarios[$key] = $c;
-                    $auxKeys[] = $key; 
-                }
-                else{
-                    $comentarios[$c['comentarioRespuesta']]['ComentRespuesta'][$key] = $c;
-                }*/
         }
         return $Objcomentarios;
-	}
-    
-
-
-	/**
-	 * Ver detalles del foro, idea principal y comentarios
-	 *
-	 * @author Camilo Quijano <camilo@altactic.com>
-     * @version 1
-     * @Route("foro_{foroId}", name="foros_show")
-	 * @Template("vocationetBundle:Foros:showForo.html.twig")
-	 *
-	 */
-	public function showForoAction($foroId)
-	{
-		$em = $this->getDoctrine()->getManager();
-        $carreras = $em->getRepository('vocationetBundle:Carreras')->findAll();
-		return array();
-	}
-	
-	/**
-	 * Función que retorna el listado de temas de una carrera y cantidad de foros por tema
-	 *
-	 * @author Camilo Quijano <camilo@altactic.com>
-     * @version 1
-     * @return Array Arreglo con datos del tema, y cantidad de foros
-	 */
-    private function getTemasCountForos($carreraId)
-    {
-		$em = $this->getDoctrine()->getManager();
-		$dql = "SELECT t.id, t.nombre, COUNT(f.id) as cantidadForos
-				FROM vocationetBundle:Temas t
-				LEFT JOIN vocationetBundle:Foros f WITH f.tema = t.id
-				WHERE t.carrera =:carreraId
-				GROUP BY t.id";
-		$query = $em->createQuery($dql);
-		$query->setParameter('carreraId', $carreraId);
-		return $query->getResult();
 	}
 
 	/**
@@ -353,6 +220,27 @@ class ForosController extends Controller
 				LEFT JOIN vocationetBundle:Comentarios comment WITH comment.foro = f.id
 				WHERE c.id =:carreraId".$auxWhere."
 				GROUP BY f.id";
+		$query = $em->createQuery($dql);
+		$query->setParameter('carreraId', $carreraId);
+		return $query->getResult();
+	}
+
+	/**
+	 * Función que retorna el listado de temas de una carrera y cantidad de foros por tema
+	 *
+	 * @author Camilo Quijano <camilo@altactic.com>
+     * @version 1
+     * @param Int $carreraId Id de la carrera
+     * @return Array Arreglo con datos del tema, y cantidad de foros
+	 */
+    private function getTemasCountForos($carreraId)
+    {
+		$em = $this->getDoctrine()->getManager();
+		$dql = "SELECT t.id, t.nombre, COUNT(f.id) as cantidadForos
+				FROM vocationetBundle:Temas t
+				LEFT JOIN vocationetBundle:Foros f WITH f.tema = t.id
+				WHERE t.carrera =:carreraId
+				GROUP BY t.id";
 		$query = $em->createQuery($dql);
 		$query->setParameter('carreraId', $carreraId);
 		return $query->getResult();
