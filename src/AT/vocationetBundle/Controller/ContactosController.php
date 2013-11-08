52df054c2346dc3250c0e370ecb30aac8bd0ffd4
CamiloQuijano <camilo@altactic.com>
2013-11-08 15:48:54 -0500

    Contactos del usuario

--- src/AT/vocationetBundle/Controller/ContactosController.php
+++ src/AT/vocationetBundle/Controller/ContactosController.php
@@ -3,6 +3,7 @@
 namespace AT\vocationetBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -35,6 +36,47 @@ class ContactosController extends Controller
         return array('contactos' => $contactos);
     }
 
+	/**
+     * PENDIENTE DOCUMENTACION AJAX POST
+     * PENDIENTE DOCUMENTACION
+     * 
+     * @Route("/estado", name="edit_estado_relacion")
+     * @return Response
+     */
+    public function EditEstadoRelacionAction(Request $request)
+    {
+		$security = $this->get('security');
+		$usuarioId = $security->getSessionValue('id');
+		
+		$usuario = $request->request->get('u');
+		$relacionId = $request->request->get('r');
+		$tipo = $request->request->get('t'); //Tipo r=>Rechazar c=>Cancelar a=>Aprobar
+
+		$em = $this->getDoctrine()->getManager();
+
+		$estadoReturn = 'error';
+		if ($tipo == 'r') {
+			$relacion = $em->getRepository('vocationetBundle:Relaciones')->findOneBy(Array('id' => $relacionId, 'usuario' => $usuario, 'usuario2' => $usuarioId, 'tipo' => 1, 'estado' => 0));
+			$estadoReturn = ($relacion) ? 'delete' : 'error';
+			if ($relacion) { $em->remove($relacion); }
+		} elseif ($tipo == 'c') {
+			$relacion = $em->getRepository('vocationetBundle:Relaciones')->findOneBy(Array('id' => $relacionId, 'tipo' => 1, 'estado' => 1));
+			$estadoReturn = ($relacion) ? 'delete' : 'error';
+			if ($relacion) { $em->remove($relacion); }
+		} elseif ($tipo == 'a') {
+			$relacion = $em->getRepository('vocationetBundle:Relaciones')->findOneBy(Array('id' => $relacionId, 'usuario' => $usuario, 'usuario2' => $usuarioId, 'tipo' => 1, 'estado' => 0));
+			$estadoReturn = ($relacion) ? 'cancelar' : 'error';
+			if ($relacion) {
+				$relacion->setEstado(1);
+				$em->persist($relacion);
+			}
+		}
+		
+		$em->flush();
+		$renderBtn =  $this->renderView('vocationetBundle:Contactos:ajaxBtn.html.twig', array('id'=> $usuario, 'relacionId' => $relacionId, 'estadoReturn' =>$estadoReturn));
+		return new response($renderBtn);
+	}
+
     /**
      * PENDIENTE DOCUMENTACION
      */
@@ -51,11 +93,12 @@ class ContactosController extends Controller
 		 * WHERE r.tipo = 1 AND  u.id != 7
 		 * 	AND (((r.usuario_id = 7 OR r.usuario2_id = 7) AND r.estado = 1)  OR  (r.usuario2_id = 7 and r.estado = 0));
 		 */
-        $dql = "SELECT u.id, u.usuarioNombre, u.usuarioApellido,
+        $dql = "SELECT u.id, u.usuarioNombre, u.usuarioApellido, u.usuarioImagen,
+						rol.nombre AS rolNombre,
 						u.usuarioProfesion,
 						c.nombre AS nombreColegio, u.usuarioCursoActual,
-					rol.nombre AS nombreRol,
-					est.nombreInstitucion
+						est.nombreInstitucion, est.titulo,
+						r.estado, r.id AS relacionId
                 FROM vocationetBundle:Usuarios u
                 JOIN u.rol rol
                 LEFT JOIN u.colegio c
@@ -67,7 +110,36 @@ class ContactosController extends Controller
                 ORDER BY r.estado, u.id";
         $query = $em->createQuery($dql);
         $query->setParameter('usuarioId', $usuarioId);
-		return $query->getResult();
+        $contactos = $query->getResult();
+        
+		$arrContactos = Array();
+        if ($contactos) {
+			$auxId = 0;
+			foreach ($contactos as $cont)
+			{
+				if ($auxId != $cont['id']) {
+					$arrContactos[$cont['id']] = Array(
+						'id' => $cont['id'],
+						'usuarioImagen' => $cont['usuarioImagen'],
+						'usuarioNombre' => $cont['usuarioNombre'],
+						'usuarioApellido' => $cont['usuarioApellido'],
+						'rolNombre' => $cont['rolNombre'],
+						'usuarioProfesion' => $cont['usuarioProfesion'],
+						'nombreColegio' => $cont['nombreColegio'],
+						'usuarioCursoActual' => $cont['usuarioCursoActual'],
+						'estado' => $cont['estado'],
+						'relacionId' => $cont['relacionId']);
+					$auxId = $cont['id'];
+				} else {
+					$arrContactos[$cont['id']]['estudios'][] = Array(
+						'nombreInstitucion' => $cont['nombreInstitucion'],
+						'titulo' => $cont['titulo']);
+				}
+			}
+		}
+
+		//return $contactos;
+		return $arrContactos;
 	}
 }
 ?>
